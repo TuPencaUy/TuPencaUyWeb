ba6b49afcb6d1e80eeb5409cb73bc95e81d810da
Shifter24 <felipenahuelviera@hotmail.com>
2024-04-30 22:58:14 -0300

    init auth0 SDK

--- src/main.js
+++ src/main.js
@@ -4,5 +4,17 @@ import "bootstrap"
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createAuth0 } from '@auth0/auth0-vue'
+import auth0_config from '../auth0_config.json'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(
+    createAuth0({
+        domain: auth0_config.domain_id,
+        clientId: auth0_config.domain_id,
+        authorizationParams: {
+            redirect_uri: window.location.origin
+        },
+    })
+)
+.mount('#app')
