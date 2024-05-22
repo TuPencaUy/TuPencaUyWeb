eadfa3f1de04238f51dd08175a2f5040841d440d
angelsebamc <asmcardozoo@gmail.com>
2024-05-21 21:53:10 -0300

    adding route

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 
 import App from '../App.vue'
 import HomeView from '../pages/Home.vue'
+import SignInView from '../pages/Signin.vue'
 import LoginView from '../pages/Login.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
+  { path: '/signin', name: 'Signin', component: SignInView },
   { path: '/login', name: 'login', component: LoginView },
   { path: '/:catchAll(.*)', name: 'Not Found', component: LoginView }
 ]
