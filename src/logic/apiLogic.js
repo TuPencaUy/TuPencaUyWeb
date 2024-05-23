9cf9295df174585a5f0ffde3f06a97760fdfd0b0
angelsebamc <asmcardozoo@gmail.com>
2024-05-23 20:06:56 -0300

    update: headers control in apiLogic

--- src/logic/apiLogic.js
+++ src/logic/apiLogic.js
@@ -1,27 +1,37 @@
-export default function api()
-{
+export default function api() {
   const BASE_URL = import.meta.env.VITE_API_CORE_URL
 
-  async function execute(url, method, body = {}, headers = {})
-  {
+  async function execute(url, method, body = {}, headers = {}) {
     const requestObject = {
       method,
       headers: {
-        'Content-Type': 'application/json',
-        ...headers
+        'Content-Type': 'application/json'
       }
     }
 
-    if (body && Object.keys(body).length > 0)
-    {
+    if (body && Object.keys(body).length > 0) {
       requestObject.body = JSON.stringify(body);
+    }
+
+    if (Object.keys(headers).length === 0) {
+      Object.assign(requestObject.headers, headers);
     }
 
     const response = await fetch(`${BASE_URL}${url}`, requestObject);
     return response;
   }
 
+  function response(data = null, message = "", error = false)
+  {
+    return {
+      error: error,
+      message: message,
+      data: data
+    }
+  }
+
   return {
     execute,
+    response
   }
 }
