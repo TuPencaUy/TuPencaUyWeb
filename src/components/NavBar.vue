36c567d1fe623ac89e6599505dd83ab1d4234cf2
Shifter24 <felipenahuelviera@hotmail.com>
2024-05-22 23:52:06 -0300

    - logo added - navbar changes and login

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { useUserStore } from '@/store/user'
 import { useRouter } from 'vue-router'
+import { useAuth0 } from '@auth0/auth0-vue'
+
+const auth0 = useAuth0();
 
 const router = useRouter()
 
@@ -11,25 +14,17 @@ const handleLogout = () => {
 </script>
 
 <template>
-  <header class=".navbar p-3 mb-3 border-bottom bg-dark" data-bs-theme="dark">
+  <header class=".navbar mb-3 border-bottom bg-dark" data-bs-theme="dark">
     <div class="container">
       <div
-        class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"
+        class="d-flex flex-wrap align-items-center justify-content-between"
       >
         <router-link
           to="/"
           class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none"
         >
-          Tu Penca UY
+          <img class="logo" src="../../public/logo.png">
         </router-link>
-
-        <ul class="nav col-12 col-lg-2 me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 link-body-emphasis">Products</a></li>
-        </ul>
-
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control" placeholder="Search..." aria-label="Search" />
-        </form>
 
         <router-link
           v-if="!useUserStore()?.isAuthenticated"
@@ -53,7 +48,7 @@ const handleLogout = () => {
             aria-expanded="false"
           >
             <img
-              src="https://github.com/mdo.png"
+              :src="auth0?.user?._value?.picture ?? ''"
               alt="mdo"
               width="32"
               height="32"
@@ -70,3 +65,9 @@ const handleLogout = () => {
     </div>
   </header>
 </template>
+
+<style scoped>
+  .logo {
+    width: 100px;
+  }
+</style>
