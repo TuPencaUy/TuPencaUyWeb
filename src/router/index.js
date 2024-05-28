7b64e8b3b45e16b80310332cb74fa6b98cb4a536
Shifter24 <felipenahuelviera@hotmail.com>
2024-05-28 20:56:30 -0300

    agrego endpoint add events

--- src/router/index.js
+++ src/router/index.js
@@ -14,13 +14,13 @@ const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/signup', name: 'Signin', component: SignUpView },
   { path: '/login', name: 'login', component: LoginView },
+  { path: '/create-site', name: 'create-site', component: CreateSiteView },
   { path: '/:catchAll(.*)', name: 'Not Found', component: LoginView },
   //Admin routes
   { path: '/admin', name: 'dashboard', component: DashboardView },
   { path: '/admin/events', name: 'events', component: EventsView },
   { path: '/admin/events/add', name: 'addEvent', component: EventsAdd },
   { path: '/admin/sports', name: 'sports', component: SportsView },
-  { path: '/create-site', name: 'create-site', component: CreateSiteView },
 ];
 
 const router = createRouter({
