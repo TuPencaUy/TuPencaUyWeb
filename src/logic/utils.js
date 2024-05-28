c353b67466c6bcd628b08c155a3df631b2dcf5be
Shifter24 <felipenahuelviera@hotmail.com>
2024-05-28 00:25:59 -0300

    feat: ensure is logged in

--- src/logic/utils.js
+++ src/logic/utils.js
@@ -1,23 +1,31 @@
-export default function utils()
-{
-    function showLoader()
-    {
+import router from "@/router/index.js";
+import {useUserStore} from "@/store/user.js";
+
+export default function utils() {
+    function showLoader() {
         const loader = document.querySelector('.loader-modal');
         if (!loader) return;
 
         loader.classList.remove('hidden');
     }
 
-    function hideLoader()
-    {
+    function hideLoader() {
         const loader = document.querySelector('.loader-modal');
         if (!loader) return;
 
         loader.classList.add('hidden');
     }
 
+    function ensureIsLoggedIn() {
+        if (!useUserStore().isAuthenticated) {
+            router.push('/login');
+        }
+
+    }
+
     return {
         showLoader,
-        hideLoader
+        hideLoader,
+        ensureIsLoggedIn
     };
 }
