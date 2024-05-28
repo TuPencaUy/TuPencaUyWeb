00527bec76ac9246e94141c28890eb2702beb94e
Shifter24 <felipenahuelviera@hotmail.com>
2024-05-28 00:31:28 -0300

    adding route dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,23 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import {createMemoryHistory, createRouter} from 'vue-router';
 
-import App from '../App.vue'
-import HomeView from '../pages/Home.vue'
-import LoginView from '../pages/LoginView.vue'
-import SignUpView from '../pages/SignupView.vue'
-import CreateSiteView from '../pages/CreateSiteView.vue'
+import App from '../App.vue';
+import HomeView from '../pages/Home.vue';
+import LoginView from '../pages/LoginView.vue';
+import SignUpView from '../pages/SignupView.vue';
+import DashboardView from "@/pages/Admin/Dashboards/DashboardView.vue";
 
 const routes = [
-  { path: '/', name: 'home', component: HomeView },
-  { path: '/signup', name: 'Signin', component: SignUpView },
-  { path: '/login', name: 'login', component: LoginView },
-  { path: '/create-site', name: 'create-site', component: CreateSiteView },
-  { path: '/:catchAll(.*)', name: 'Not Found', component: LoginView }
-]
+    {path: '/', name: 'home', component: HomeView},
+    {path: '/signup', name: 'Signin', component: SignUpView},
+    {path: '/login', name: 'login', component: LoginView},
+    {path: '/:catchAll(.*)', name: 'Not Found', component: LoginView},
+    //Admin routes
+    {path: '/admin', name: 'dashboard', component: DashboardView},
+];
 
 const router = createRouter({
-  history: createMemoryHistory(),
-  routes
-})
+    history: createMemoryHistory(),
+    routes
+});
 
-export default router
+export default router;
