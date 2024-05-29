9d1258e2d92b8f8551eac6b974f57b18741d4665
angelsebamc <asmcardozoo@gmail.com>
2024-05-28 23:03:28 -0300

    create sport view

---
+++ src/pages/Admin/Sports/SportsView.vue
@@ -0,0 +1,67 @@
+<script setup>
+import Admin from "@/components/Admin.vue";
+import { onMounted, ref } from "vue";
+import utils from "@/logic/utils.js";
+import router from "@/router/index.js";
+import { Icon } from "@iconify/vue";
+import sportLogic from "@/logic/sportLogic";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import Button from "@/components/ui/button/Button.vue";
+
+if (!utils().ensureIsLoggedIn()) {
+    router.push('/login');
+}
+
+const collection = ref([]);
+
+onMounted(async () => {
+    const response = await sportLogic().getSports();
+    if (response && response?.data) {
+        collection.value = response.data?.list;
+    }
+});
+
+function deleteItem() {
+    console.log('delete item');
+}
+</script>
+
+<template>
+    <Admin title="Sports" name-btn-add="Add Sport" path-to-add="/admin/sports/add">
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>ID</TableHead>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Accepts Tie?</TableHead>
+                    <TableHead>Exact Points</TableHead>
+                    <TableHead>Partial Points</TableHead>
+                    <TableHead class="text-right">
+                    </TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow v-for="item in collection" :key="item.id">
+                    <TableCell>{{ item.id }}</TableCell>
+                    <TableCell>{{ item.name }}</TableCell>
+                    <TableCell>{{ item.tie ? 'Yes' : 'No' }}</TableCell>
+                    <TableCell>{{ item.exactPoints }}</TableCell>
+                    <TableCell>{{ item.partialPoints }}</TableCell>
+                    <TableCell>
+                        <Button @click="deleteItem" variant="ghost"><Icon icon="radix-icons:pencil-2" class="w-4 h-4 mr-2" /></Button>
+                        <Button @click="deleteItem" variant="ghost"><Icon icon="octicon:trash-24" class="w-4 h-4 mr-2" /></Button>
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    </Admin>
+</template>
