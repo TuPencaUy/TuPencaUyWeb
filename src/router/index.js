2f14c0d8cd0cf3601ef00dd0e0d4b3d09fde09c0
angelsebamc <asmcardozoo@gmail.com>
2024-05-28 21:14:34 -0300

    add create vue route

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,13 @@ import LoginView from '../pages/LoginView.vue';
 import SignUpView from '../pages/SignupView.vue';
 import DashboardView from "@/pages/Admin/Dashboards/DashboardView.vue";
 import EventsView from "@/pages/Admin/Events/EventsView.vue";
+import CreateSiteView from '@/pages/CreateSiteView.vue';
 
 const routes = [
     {path: '/', name: 'home', component: HomeView},
     {path: '/signup', name: 'Signin', component: SignUpView},
     {path: '/login', name: 'login', component: LoginView},
+    {path: '/create-site', name: 'CreateSite', component: CreateSiteView},
     {path: '/:catchAll(.*)', name: 'Not Found', component: LoginView},
     //Admin routes
     {path: '/admin', name: 'dashboard', component: DashboardView},
