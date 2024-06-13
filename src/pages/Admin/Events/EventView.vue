7991e33a777a21299f0d0eaee35ec9237eca56cc
Angel <asmcardozoo@gmail.com>
2024-06-13 13:44:54 -0300

    update create method yo accept update action too

--- src/pages/Admin/Events/EventView.vue
+++ src/pages/Admin/Events/EventView.vue
@@ -39,11 +39,13 @@ import utils from '@/logic/utils';
 
 const route = useRoute();
 
+let eventId = '';
+
 const eventData = ref({
   name: 'eventName',
   startDate: '',
   endDate: '',
-  commission: 'no',
+  comission: 'no',
   teamType: 'local',
 });
 
@@ -58,13 +60,18 @@ const {handleSubmit} = useForm({
 });
 
 onMounted(async () => {
-  const eventId = route.params.id;
-  debugger
+  eventId = String(route.params.id);
   if(eventId) {
     utils().showLoader();
     const response = await eventsLogic().getEvents(eventId);
     if (response && response?.data) {
       eventData.value = response.data;
+
+      console.log(eventData.value);
+      eventData.value.comission = String(eventData.value.comission) === '0' ? 'no' : 'yes';
+      eventData.value.teamType = String(eventData.value.teamType) === '1' ? 'national' : 'local';
+      eventData.value.startDate = eventData.value.startDate.split('T')[0];
+      eventData.value.endDate = eventData.value.endDate.split('T')[0];
     }
 
     setTimeout(() => {
@@ -75,12 +82,13 @@ onMounted(async () => {
 
 const onSubmit = handleSubmit(async () => {
   utils().showLoader();
-  const response = await eventsLogic().createEvent(eventData?.value);
+  const response = await eventsLogic().createOrUpdateEvent(eventData?.value, eventId);
   utils().hideLoader();
+
   if (response && !response?.error) {
     toast({
-      title: 'Event created',
-      description: 'Event has been created successfully',
+      title: `Event ${eventId !== null ? "updated" : "created"}`,
+      description: `Event has been ${eventId !== null ? "updated" : "created"} successfully`,
     });
     setTimeout(() => {
       router.push('/admin/events');
@@ -172,7 +180,7 @@ const onSubmit = handleSubmit(async () => {
                     <div class="grid gap-6">
                       <div class="grid gap-3">
                         <Label for="status">Commission</Label>
-                        <Select v-model="eventData.commission">
+                        <Select v-model="eventData.comission">
                           <SelectTrigger id="status" aria-label="Select commission">
                             <SelectValue placeholder="Select commission"/>
                           </SelectTrigger>
