5bc361f05ed081efc8fe118df23b4d8ea9925b31
Shifter24 <felipenahuelviera@hotmail.com>
2024-06-13 22:33:14 -0300

    Add subscribeToEvent endpoint

--- src/logic/userLogic.js
+++ src/logic/userLogic.js
@@ -1,47 +1,52 @@
-import api from './apiLogic'
+import api from './apiLogic';
+import {useUserStore} from "@/store/user.js";
+import {useTenantStore} from "@/store/tenant.js";
 
-export default function userLogic()
-{
-  async function basicSignUp(userData, currentTenant)
-  {
-    try
-    {
-      const response = await api().execute('/identity/basicsignup', 'POST', userData, { currentTenant });
-      return await response.json();
+export default function userLogic() {
+    async function basicSignUp(userData, currentTenant) {
+        try {
+            const response = await api().execute('/identity/basicsignup', 'POST', userData, {currentTenant});
+            return await response.json();
 
-    } catch (error)
-    {
-      return null;
+        } catch (error) {
+            return null;
+        }
     }
-  }
 
-  async function basicLogin(userData)
-  {
-    try
-    {
-      const response = await api().execute('/identity/basiclogin', 'POST', userData);
-      return await response.json();
-    } catch (error)
-    {
-      return error;
+    async function basicLogin(userData) {
+        try {
+            const currentTenant = useTenantStore().getCurrentTenant;
+            const response = await api().execute('/identity/basiclogin', 'POST', userData, {currentTenant});
+            return await response.json();
+        } catch (error) {
+            return error;
+        }
     }
-  }
 
-  async function authLogin(token, currentTenant = {})
-  {
-    try
-    {
-      const response = await api().execute('/identity/oauthlogin', 'POST', token, { currentTenant });
-      return await response.json();
-    } catch (error)
-    {
-      return error;
+    async function authLogin(token, currentTenant = {}) {
+        try {
+            const response = await api().execute('/identity/oauthlogin', 'POST', token, {currentTenant});
+            return await response.json();
+        } catch (error) {
+            return error;
+        }
     }
-  }
 
-  return {
-    basicSignUp,
-    basicLogin,
-    authLogin
-  }
+    async function subscribeToEvent(eventId) {
+        try {
+            const currentTenant = useTenantStore().getCurrentTenant;
+            const userId = useUserStore()._user?.id;
+            const response = await api().execute(`/user/subscribetoevent?userId=${userId}&eventId=${eventId}`, 'POST', null, {currentTenant, "Authorization": `Bearer ${useUserStore().getToken}`});
+            return await response.json();
+        } catch (error) {
+            return error;
+        }
+    }
+
+    return {
+        basicSignUp,
+        basicLogin,
+        authLogin,
+        subscribeToEvent,
+    };
 }
