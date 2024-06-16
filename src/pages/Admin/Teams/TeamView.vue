58ba2f8de99ff3348f25d1c97e5f27b4adf00670
Shifter24 <felipenahuelviera@hotmail.com>
2024-06-16 03:06:53 -0300

    Add teams crud

--- src/pages/Admin/Teams/TeamView.vue
+++ src/pages/Admin/Teams/TeamView.vue
@@ -8,12 +8,9 @@ import {Input} from '@/components/ui/input';
 import {Label} from '@/components/ui/label';
 import teamLogic from '@/logic/teamLogic';
 import sportLogic from '@/logic/sportLogic';
-import { useToast } from '@/components/ui/toast/use-toast'
-import { Toaster } from '@/components/ui/toast'
-import router from '@/router'
-import { useRoute } from 'vue-router'
-
-const { toast } = useToast()
+import {useToast} from '@/components/ui/toast/use-toast';
+import router from '@/router';
+import {useRoute} from 'vue-router';
 import {
   Select,
   SelectContent,
@@ -28,25 +25,25 @@ import
   FormItem,
   FormLabel,
   FormMessage,
-} from '@/components/ui/form'
+} from '@/components/ui/form';
 import Admin from "@/components/Admin.vue";
 import {onMounted, ref} from "vue";
 import {toTypedSchema} from "@vee-validate/zod";
 import * as z from "zod";
-import {useForm} from 'vee-validate'
+import {useForm} from 'vee-validate';
 import utils from '@/logic/utils';
 
-
 const route = useRoute();
+const {toast} = useToast();
 
-let objectId = '';
+let teamId = '';
 let sports = ref([]);
 
 const objectData = ref({
   name: 'Team name',
-  logo: 'no',
-  teamType: 0,
-  sport: 0,
+  logo: '',
+  teamType: '1',
+  sport: '1',
 });
 
 const formSchema = toTypedSchema(z.object({
@@ -58,12 +55,16 @@ const {handleSubmit} = useForm({
 });
 
 onMounted(async () => {
-  objectId = route.params.id ?? '';
-  if(objectId) {
+  teamId = route.params.id ?? '';
+  if (teamId) {
     utils().showLoader();
-    const response = await teamLogic().getTeams(objectId);
+    const response = await teamLogic().getTeam(teamId);
     if (response && response?.data) {
-      objectData.value = response.data;
+      objectData.value.name = response.data.name;
+      objectData.value.logo = response.data.logo;
+      objectData.value.teamType = String(response.data.teamType);
+      objectData.value.sport = response.data.sport;
+      objectData.value.sport.id = String(response.data.sport?.id);
 
       setTimeout(() => {
         utils().hideLoader();
@@ -72,20 +73,22 @@ onMounted(async () => {
   }
 
   //get sports
-  sports = await sportLogic().getSports();
-  sports.value = sports.data.list.map(elem =>  { return {id: elem.id, name: elem.name}});
+  const sportsResponse = await sportLogic().getSports();
+  sports.value = sportsResponse.data.list.map(elem => {
+    return {id: String(elem.id), name: elem.name};
+  });
 });
 
 const onSubmit = handleSubmit(async () => {
   utils().showLoader();
-  
-  const response = await teamLogic().createOrUpdateTeam(objectData?._rawValue, objectId);
+
+  const response = await teamLogic().createOrUpdateTeam(objectData?._rawValue, teamId);
   utils().hideLoader();
 
   if (response && !response?.error) {
     toast({
-      title: `Team ${objectId !== '' ? "updated" : "created"}`,
-      description: `Team has been ${objectId !== '' ? "updated" : "created"} successfully`,
+      title: `Team ${teamId !== '' ? "updated" : "created"}`,
+      description: `Team has been ${teamId !== '' ? "updated" : "created"} successfully`,
     });
     setTimeout(() => {
       router.push('/admin/teams');
@@ -98,50 +101,62 @@ const onSubmit = handleSubmit(async () => {
     });
   }
 });
+
+const onLogoChanged = (e) => {
+  const file = e.target.files[0];
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    objectData.value.logo = e.target.result;
+  };
+  reader.readAsDataURL(file);
+};
 </script>
 
 <template>
-  <Toaster />
   <Admin>
     <div class="flex w-full flex-col bg-muted/40">
       <div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-          <div class="flex flex-col">
-            <div class="flex items-center gap-4">
-              <router-link to="/admin/teams">
-                <Button variant="outline" size="icon" class="h-7 w-7">
-                  <ChevronLeft class="h-4 w-4"/>
-                </Button>
+        <div class="flex flex-col">
+          <div class="flex items-center gap-4">
+            <router-link to="/admin/teams">
+              <Button variant="outline" size="icon" class="h-7 w-7">
+                <ChevronLeft class="h-4 w-4"/>
+              </Button>
+            </router-link>
+            <h1 class="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
+              {{ objectData.name }}
+            </h1>
+            <div class="hidden items-center gap-2 md:ml-auto md:flex">
+              <router-link variant="outline" class="inline-block" to="/admin/teams">
+                Discard
               </router-link>
-              <h1 class="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
-                {{ objectData.name }}
-              </h1>
-              <div class="hidden items-center gap-2 md:ml-auto md:flex">
-                <router-link variant="outline" class="inline-block" to="/admin/teams">
-                    Discard
-                </router-link>
-                <Button @click="onSubmit" size="sm">
-                  Save
-                </Button>
-              </div>
+              <Button @click="onSubmit" size="sm">
+                Save
+              </Button>
             </div>
-            <div class="flex w-full mt-2">
-              <div class="w-full">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Create Team</CardTitle>
-                  </CardHeader>
-                  <CardContent>
+          </div>
+          <div class="flex w-full mt-2">
+            <div class="w-full">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Create Team</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div class="grid gap-6">
+                    <FormField v-slot="{ componentField }" name="name">
+                      <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                          <Input type="text" v-bind="componentField" v-model="objectData.name"/>
+                        </FormControl>
+                        <FormMessage/>
+                      </FormItem>
+                    </FormField>
+                    <Label for="logo">Logo</Label>
+                    <Input name="logo" v-if="!teamId || teamId && !objectData.logo" type="file" @change="onLogoChanged"/>
+                    <img v-if="objectData.logo && !objectData.logo?.includes('data:image')" :src="['data:image/png;base64', objectData.logo]" alt="logo1" class="w-80 h-80">
+                    <img v-else-if="objectData.logo" :src="objectData.logo" alt="logo2" class="w-80 h-80">
                     <div class="grid gap-6">
-                      <FormField v-slot="{ componentField }" name="name">
-                        <FormItem>
-                          <FormLabel>Name</FormLabel>
-                          <FormControl>
-                            <Input type="text" v-bind="componentField" v-model="objectData.name"/>
-                          </FormControl>
-                          <FormMessage/>
-                        </FormItem>
-                      </FormField>
-                      <div class="grid gap-6">
                       <div class="grid gap-3">
                         <Label for="status">Team Type</Label>
                         <Select v-model="objectData.teamType">
@@ -159,35 +174,45 @@ const onSubmit = handleSubmit(async () => {
                         </Select>
                       </div>
                     </div>
-                      <div class="grid gap-6">
-                        <div class="grid gap-3">
-                          <Label for="status">Sports</Label>
-                          <Select v-model="objectData.sport">
-                            <SelectTrigger id="status" aria-label="Sports">
-                              <SelectValue placeholder="Sport"/>
-                            </SelectTrigger>
-                            <SelectContent>
-                              <SelectItem v-for="sport in sports.value" class="bg-white" :value="sport.id">
-                                {{ sport.name }}
-                              </SelectItem>
-                            </SelectContent>
-                          </Select>
-                        </div>
+                    <div class="grid gap-6">
+                      <div class="grid gap-3">
+                        <Label for="status">Sports</Label>
+                        <Select v-if="teamId" v-model="objectData.sport.id">
+                          <SelectTrigger id="status" aria-label="Sports">
+                            <SelectValue placeholder="Select sport"/>
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem v-for="sport in sports" class="bg-white" :value="sport.id">
+                              {{ sport.name }}
+                            </SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <Select v-else-if="!teamId" v-model="objectData.sport">
+                          <SelectTrigger id="status" aria-label="Sports">
+                            <SelectValue placeholder="Select sport"/>
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem v-for="sport in sports" class="bg-white" :value="sport.id">
+                              {{ sport.name }}
+                            </SelectItem>
+                          </SelectContent>
+                        </Select>
                       </div>
                     </div>
-                  </CardContent>
-                </Card>
-              </div>
-            </div>
-            <div class="flex items-center justify-center gap-2 md:hidden">
-              <router-link variant="outline" class="inline-block" to="/admin/teams">
-                  Discard
-              </router-link>
-              <Button @click="onSubmit" size="sm">
-                Save
-              </Button>
+                  </div>
+                </CardContent>
+              </Card>
             </div>
           </div>
+          <div class="flex items-center justify-center gap-2 md:hidden">
+            <router-link variant="outline" class="inline-block" to="/admin/teams">
+              Discard
+            </router-link>
+            <Button @click="onSubmit" size="sm">
+              Save
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   </Admin>
--- src/pages/Admin/Teams/TeamsView.vue
+++ src/pages/Admin/Teams/TeamsView.vue
@@ -1,20 +1,17 @@
 <script setup>
 import Admin from "@/components/Admin.vue";
-import { onMounted, ref } from "vue";
+import {onMounted, ref} from "vue";
 import utils from "@/logic/utils.js";
-import router from "@/router/index.js";
-import { Icon } from "@iconify/vue";
+import {Icon} from "@iconify/vue";
 import teamLogic from "@/logic/teamLogic";
-
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from '@/components/ui/table'
-
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import {
   AlertDialog,
   AlertDialogAction,
@@ -25,86 +22,108 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from '@/components/ui/alert-dialog'
+} from '@/components/ui/alert-dialog';
 
 import Button from "@/components/ui/button/Button.vue";
+import {useToast} from "@/components/ui/toast/index.js";
 
 const collection = ref([]);
 
+const {toast} = useToast();
+
 onMounted(async () => {
-    utils().showLoader();
-    const response = await teamLogic().getTeams();
-    if (response && response?.data) {
-        collection.value = response.data?.list;
-    }
-    setTimeout(() => {
-        utils().hideLoader();
-    }, 1000);
+  utils().showLoader();
+  const teams = await teamLogic().getTeams();
+  if (teams && teams.length > 0) {
+    collection.value = teams;
+  }
+  setTimeout(() => {
+    utils().hideLoader();
+  }, 1000);
 });
 
-function deleteItem() {
-    console.log('delete item');
+async function deleteItem(id) {
+  utils().showLoader();
+  const response = await teamLogic().deleteTeam(id);
+  utils().hideLoader();
+  if (response && !response?.error) {
+    toast({
+      title: 'Team deleted',
+      description: 'Team has been deleted successfully',
+    });
+    const teams = await teamLogic().getTeams();
+    if (teams && teams.length > 0) {
+      collection.value = teams;
+    }
+  } else {
+    toast({
+      title: 'Error',
+      description: 'An error occurred',
+      variant: 'destructive',
+    });
+  }
 }
 </script>
 
 <template>
-    <Admin title="Teams" name-btn-add="Add Team" path-to-add="/admin/teams/add">
-        <div class="w-full m-auto" v-if="collection.length < 1">
-            <div class="text-center">
-                <h3 class="text-2xl font-bold tracking-tight">
-                    You have no teams
-                </h3>
-                <p class="text-sm text-muted-foreground">
-                    You can start by adding a new one
-                </p>
-            </div>
-        </div>
-        <Table v-else>
-            <TableHeader>
-                <TableRow>
-
-                    <TableHead>Logo</TableHead>
-                    <TableHead>ID</TableHead>
-                    <TableHead>Name</TableHead>
-                    <TableHead>Team Type</TableHead>
-                    <TableHead>Sport</TableHead>
-                    <TableHead class="text-right">
-                    </TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                <TableRow v-for="item in collection" :key="item.id">
-                    <TableCell>{{ item.logo }}</TableCell>
-                    <TableCell>{{ item.id }}</TableCell>
-                    <TableCell>{{ item.name }}</TableCell>
-                    <TableCell>{{ item.teamType }}</TableCell>
-                    <TableCell>{{ item.sport.name }}</TableCell>
-                    <TableCell>
-                        <router-link class="inline-block" :to="`/admin/teams/${item.id}`">
-                            <Icon icon="radix-icons:pencil-2" class="w-4 h-4 mr-2" />
-                        </router-link>
-                        <AlertDialog>
-                        <AlertDialogTrigger as-child>
-                            <Button variant="ghost">
-                            <Icon icon="octicon:trash-24" class="w-4 h-4 mr-2" />
-                            </Button>
-                        </AlertDialogTrigger>
-                        <AlertDialogContent>
-                            <AlertDialogHeader>
-                            <AlertDialogTitle>Are you sure to delete it?</AlertDialogTitle>
-                            <AlertDialogDescription>
-                                This action cannot be undone. This will permanently delete the sport.
-                            </AlertDialogDescription>
-                            </AlertDialogHeader>
-                            <AlertDialogFooter>
-                            <AlertDialogCancel>Cancel</AlertDialogCancel>
-                            <AlertDialogAction @click="deleteItem(item.id)">Continue</AlertDialogAction>
-                            </AlertDialogFooter>
-                        </AlertDialogContent>
-                        </AlertDialog>
-                    </TableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
-    </Admin>
+  <Admin title="Teams" name-btn-add="Add Team" path-to-add="/admin/teams/add">
+    <div class="w-full m-auto" v-if="collection.length < 1">
+      <div class="text-center">
+        <h3 class="text-2xl font-bold tracking-tight">
+          You have no teams
+        </h3>
+        <p class="text-sm text-muted-foreground">
+          You can start by adding a new one
+        </p>
+      </div>
+    </div>
+    <Table v-else>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Logo</TableHead>
+          <TableHead>ID</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Team Type</TableHead>
+          <TableHead>Sport</TableHead>
+          <TableHead class="text-right">
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow v-for="item in collection" :key="item.id">
+          <TableCell>
+            <img v-if="item.logo" :src="['data:image/png;base64',item.logo]" alt="logo" class="w-40 h-40"/>
+          </TableCell>
+          <TableCell>{{ item.id }}</TableCell>
+          <TableCell>{{ item.name }}</TableCell>
+          <TableCell>{{ item.teamType }}</TableCell>
+          <TableCell>{{ item.sport.name }}</TableCell>
+          <TableCell>
+            <router-link class="inline-block" :to="`/admin/teams/${item.id}`">
+              <Icon icon="radix-icons:pencil-2" class="w-4 h-4 mr-2"/>
+            </router-link>
+            <AlertDialog>
+              <AlertDialogTrigger as-child>
+                <Button variant="ghost">
+                  <Icon icon="octicon:trash-24" class="w-4 h-4 mr-2"/>
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you sure to delete it?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete the sport.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction @click="deleteItem(item.id)">Continue</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </Admin>
 </template>
