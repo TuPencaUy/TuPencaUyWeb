b53798da0c5e23554ee0086595ce356197e8dd4e
Shifter24 <felipenahuelviera@hotmail.com>
2024-07-09 00:31:30 -0300

    Fix general api requests

--- src/logic/apiLogic.js
+++ src/logic/apiLogic.js
@@ -1,43 +1,46 @@
 export default function api() {
-  const BASE_URL = import.meta.env.VITE_API_CORE_URL
+    const BASE_URL = import.meta.env.VITE_API_CORE_URL;
 
-  async function execute(url, method, body = {}, headers = {}) {
-    const requestObject = {
-      method,
-      headers: {
-        'Content-Type': 'application/json'
-      }
+    async function execute(url, method, body = {}, headers = {}, isExternal = false) {
+        const requestObject = {
+            method,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        if (body && Object.keys(body).length > 0) {
+            requestObject.body = JSON.stringify(body);
+        }
+
+
+        const filteredHeaders = Object.fromEntries(
+            Object.entries(headers).filter(([key, value]) => value !== null)
+        );
+
+        if (Object.keys(filteredHeaders).length > 0) {
+            Object.assign(requestObject.headers, filteredHeaders);
+        }
+
+        let finalUrl = `${BASE_URL}${url}`;
+
+        if (isExternal) {
+            finalUrl = url;
+        }
+
+      return await fetch(finalUrl, requestObject);
     }
-    
-    if (body && Object.keys(body).length > 0) {
-      requestObject.body = JSON.stringify(body);
+
+    function response(data = null, message = "", error = false) {
+        return {
+            error: error,
+            message: message,
+            data: data
+        };
     }
 
-
-    const filteredHeaders = Object.fromEntries(
-      Object.entries(headers).filter(([key, value]) => value !== null)
-    );
-
-    if (Object.keys(filteredHeaders).length > 0) {
-      Object.assign(requestObject.headers, filteredHeaders);
-    }
-
-    const finalUrl = `${BASE_URL}${url}`;
-
-    const response = await fetch(finalUrl, requestObject);
-    return response;
-  }
-
-  function response(data = null, message = "", error = false) {
     return {
-      error: error,
-      message: message,
-      data: data
-    }
-  }
-
-  return {
-    execute,
-    response
-  }
+        execute,
+        response
+    };
 }
