54dc1bf09f19b5f4e8c3c478adc06d211b867c03
Angel <asmcardozoo@gmail.com>
2024-07-10 11:13:29 -0300

    add charts donut and area

--- src/pages/Admin/Dashboards/DashboardView.vue
+++ src/pages/Admin/Dashboards/DashboardView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Admin from "@/components/Admin.vue";
+import Donut from "../Analytics/Donut.vue";
 </script>
 
 <template>
   <Admin title="Dashboard" nameBtnAdd="" pathToAdd="">
     <h1>Esto es el dashboard</h1>
+    <Donut></Donut>
   </Admin>
 </template>
