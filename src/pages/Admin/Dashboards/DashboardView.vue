4143737fc7ef7b4cbace8f632948255d4f813b3d
Angel <asmcardozoo@gmail.com>
2024-07-12 11:13:24 -0300

    control if dashboard is in site

--- src/pages/Admin/Dashboards/DashboardView.vue
+++ src/pages/Admin/Dashboards/DashboardView.vue
@@ -4,19 +4,22 @@ import Donut from "@/components/Analytics/Donut.vue";
 import Leaderboard from "@/components/Analytics/Leaderboard.vue";
 import chartsLogic from "@/logic/chartsLogic";
 import { onMounted, ref } from "vue";
+import { useTenantStore } from '@/store/tenant';
 
 const betsPerEvent = ref([]);
 const usersPerEvent = ref([]);
 
 onMounted(async () => {
-  betsPerEvent.value = await chartsLogic().betsPerEvent();
-  usersPerEvent.value = await chartsLogic().usersPerEvent();
+  if (!useTenantStore().isCentralSite) {
+    betsPerEvent.value = await chartsLogic().betsPerEvent();
+    usersPerEvent.value = await chartsLogic().usersPerEvent();
+  }
 });
 </script>
 
 <template>
   <Admin title="Dashboard">
-    <div class="flex flex-col p-10 w-full gap-4">
+    <div class="flex flex-col p-10 w-full gap-4" v-if="!useTenantStore().isCentralSite">
       <div class="flex justify-center p-2 w-full gap-4">
         <div class="flex rounded-lg border border-separate shadow-sm p-4 h-fit">
           <Donut title="Bets per Event" :data="betsPerEvent"></Donut>
