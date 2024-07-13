a1c18273cb7df0bab89ea3c4b0eae137eae8e4bf
Shifter24 <felipenahuelviera@hotmail.com>
2024-07-13 20:41:13 -0300

    fix endpoint to create payment

--- src/logic/paymentLogic.js
+++ src/logic/paymentLogic.js
@@ -1,5 +1,6 @@
 import api from './apiLogic';
 import {useTenantStore} from "@/store/tenant.js";
+import {useUserStore} from "@/store/user.js";
 
 export default function paymentLogic() {
 
@@ -8,7 +9,7 @@ export default function paymentLogic() {
     async function createPayment(paymentDetails) {
         try {
             const currentTenant = useTenantStore().getCurrentTenant;
-            const response = await api().execute(URL, 'POST', paymentDetails, {currentTenant});
+            const response = await api().execute(URL, 'POST', paymentDetails, {currentTenant, Authorization: `Bearer ${useUserStore().getToken}`});
             return response.json();
         } catch (error) {
             return error;
