70134fe2fcdd10ec8eae1631b31abaabd6b087e7
Shifter24 <felipenahuelviera@hotmail.com>
2024-07-16 00:20:48 -0300

    only brings events that are available and not finished

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -41,6 +41,7 @@ onMounted(async () => {
 
   paypal = await paypalLogic().initPaypal();
   events.value = await eventsLogic().getEvents();
+  events.value = events.value.filter(event => !event?.finished);
   if (userEvents?.length > 0) {
     events.value = events.value.filter(event => !userEvents.find(userEvent => userEvent.id === event.id));
   }
