f0ec7bab0631ef9f323fce09edc5c320ddf59112
Angel <asmcardozoo@gmail.com>
2024-07-16 00:31:59 -0300

    creating finance modal

--- src/components/Analytics/FinanceOverviewTable.vue
+++ src/components/Analytics/FinanceOverviewTable.vue
@@ -2,7 +2,11 @@
 import { onMounted, ref } from "vue";
 import utils from "@/logic/utils.js";
 import analyticsLogic from "@/logic/analyticsLogic.js";
+import app from '@/eventBus.js';
 
+import FinanceTableModal from "@/components/Analytics/FinanceTableModal.vue";
+
+import { Button } from "@/components/ui/button/index.js";
 import {
   Table,
   TableBody,
@@ -14,6 +18,8 @@ import {
 
 const events = ref([]);
 const sites = ref([]);
+const data = ref([]);
+const title = ref('');
 const total = ref(0);
 
 onMounted(async () => {
@@ -30,12 +36,43 @@ onMounted(async () => {
   }, 1000);
 });
 
+async function showCompleteTable(type) {
+  switch (type) {
+    case 'site':
+      title.value = 'Site Finances';
+      data.value = [...sites.value];
+      data.value = data.value.map(elem => {
+        return {
+          ...elem,
+          name: elem.siteName,
+          id: elem.siteId
+        };
+      });
+      break;
+    case 'event':
+      title.value = 'Event Finances';
+      data.value = [...events.value];
+      data.value = data.value.map(elem => {
+        return {
+          ...elem,
+          name: elem.eventName,
+          id: elem.eventId
+        };
+      });
+      break;
+  }
+
+  app.config.globalProperties.emitter.emit('showFinanceTableModal');
+}
 </script>
 
 <template>
   <div class="flex justify-around">
     <div class="my-10 p-10 border rounded w-[49%]">
-      <h3 class="text-lg font-semibold mb-4 md:text-xl">Event Finances</h3>
+      <div class="flex gap-2 justify-between mb-4">
+        <h3 class="text-lg font-semibold md:text-xl">Event Finances Last Inputs</h3>
+        <Button @click="showCompleteTable('event')">Ver Todo</Button>
+      </div>
       <Table v-if="events.length > 0">
         <TableHeader>
           <TableRow>
@@ -44,7 +81,7 @@ onMounted(async () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow v-for="item in events" :key="item.eventId">
+          <TableRow v-for="item in events.slice(0, 5)" :key="item.eventId">
             <TableCell>{{ item.eventName }}</TableCell>
             <TableCell>${{ item.totalRaised }}</TableCell>
           </TableRow>
@@ -53,7 +90,10 @@ onMounted(async () => {
     </div>
 
     <div class="my-10 p-10 border rounded w-[49%]">
-      <h3 class="text-lg font-semibold mb-4 md:text-xl">Site Finances</h3>
+      <div class="flex gap-2 justify-between mb-4">
+        <h3 class="text-lg font-semibold md:text-xl">Site Finances Last Inputs</h3>
+        <Button @click="showCompleteTable('site')">Ver Todo</Button>
+      </div>
       <Table v-if="sites.length > 0">
         <TableHeader>
           <TableRow>
@@ -62,7 +102,7 @@ onMounted(async () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow v-for="item in sites" :key="item.siteId">
+          <TableRow v-for="item in sites.slice(0, 5)" :key="item.siteId">
             <TableCell>{{ item.siteName }}</TableCell>
             <TableCell>${{ item.totalRaised }}</TableCell>
           </TableRow>
@@ -70,4 +110,6 @@ onMounted(async () => {
       </Table>
     </div>
   </div>
+
+  <FinanceTableModal :title="title" :data="data"></FinanceTableModal>
 </template>
