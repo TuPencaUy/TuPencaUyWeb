8be18b58efc3b83699a837bb44e12a1b7b616b54
Shifter24 <felipenahuelviera@hotmail.com>
2024-07-17 01:37:42 -0300

    adds security break

--- src/logic/apiLogic.js
+++ src/logic/apiLogic.js
@@ -7,7 +7,8 @@ export default function api() {
             headers: {
                 'Content-Type': 'application/json',
                 'Content-Security-Policy': 'upgrade-insecure-requests',
-            }
+            },
+            referrerPolicy: 'unsafe-url',
         };
 
         if (body && Object.keys(body).length > 0) {
